d69748dbae220d56096f1b95755462df69d157a7
zkw9803 <87410834+zkw9803@users.noreply.github.com>
2021-07-28 22:51:47 +0800

    1.0zkw

--- vue_client8082/src/components/school/student_admin.vue
+++ vue_client8082/src/components/school/student_admin.vue
@@ -7,7 +7,9 @@
         <el-button type="info" icon="el-icon-upload"  plain>导入</el-button>
         <el-button type="warning" icon="el-icon-down"  plain>导出</el-button>
       </el-row>
-    </div></el-col>
+    </div>
+      <div> <UploadExcelComponent :on-success="handleSuccess" :before-upload="beforeUpload"/></div>
+    </el-col>
 
     <el-col :span="24"><div class="grid-content bg-purple-dark">
       <div><router-view></router-view></div>
@@ -23,6 +25,7 @@
         </el-table-column>
         <el-table-column
           prop="user_name"
+
           label="用户名"
           width="100">
         </el-table-column>
@@ -111,20 +114,79 @@
 </template>
 
 <script>
+import UploadExcelComponent from '../school/cs.vue'
 export default {
   name: 'studentTable',
+  components: { UploadExcelComponent },
   data () {
     return {
+      userList:[{}],
       tableData: [{
 
-      }]
+      }],
+      tableHeader: []
     }
   },
   methods: {
     handleClick (row) {
       console.log(row)
     },
-  }
+    beforeUpload(file) {
+      const isLt1M = file.size / 1024 / 1024 < 1
+      if (isLt1M) {
+        return true
+      }
+      this.$message({
+        message: 'Please do not upload files larger than 1m in size.',
+        type: 'warning'
+      })
+      return false
+    },
+
+    handleSuccess({ results, header }) {
+      this.tableData = results
+      this.tableHeader = header
+      var user={};
+      results.forEach((item,index,array)=>{
+        user.user_id=item.序号;
+        user.user_name=item.用户名;
+        user.user_pwd=item.密码;
+        user.user_id_card=item.身份证;
+        user.user_nation=item.民族;
+        user.user_sex=item.性别;
+        user.user_age=item.年龄;
+        user.user_date_birth=item.出生年月;
+        user.user_telephone=item.联系电话;
+        user.user_mail=item.邮箱;
+        user.user_school_name=item.学校名称;
+        user.user_major=item.专业;
+        user.user_education=item.学历;
+        user.user_residence=item.居住地;
+        user.user_graduation_time=item.毕业时间;
+
+        this.userList[index]=user;
+        user={};
+      });
+      console.log("00000",this.userList);
+      this.$axios.post("/school/getSuser",
+        this.$qs.stringify({
+          userList:this.userList
+        })
+      ).then(response=>{
+        console.log(response)
+        if (response.data == "1"){
+          this.$message({
+            message: '导入成功',
+            type: '1'
+          });
+        }else {
+          this.$message.error('导入失败');
+        }
+      }).catch(err=> {
+        console.log(err)
+      })
+    },
+  },
 }
 </script>
 
