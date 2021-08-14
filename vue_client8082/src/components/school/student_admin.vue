ab91ce47e9fb2fc8ff23a7aa11dfd8b1a8f52c7e
zkw9803 <87410834+zkw9803@users.noreply.github.com>
2021-08-14 14:31:51 +0800

    8.14

--- vue_client8082/src/components/school/student_admin.vue
+++ vue_client8082/src/components/school/student_admin.vue
@@ -2,9 +2,11 @@
   <div style="width: 100%">
   <el-row>
     <el-col :span="20"><div class="grid-content bg-purple-dark" >
+      <span style="float: left;line-height: 50px;font-size: 30px;margin-left: 30px">高校管理</span>
+      <el-button type="primary" round @click="exitLogin" style="float: left;margin:10px 0px 0px 1000px">退出登录</el-button>
     </div></el-col>
 <!--    左侧菜单-->
-    <el-col :span="6"><div class="grid-content bg-purple-dark">
+    <el-col :span="6"><div class="grid-content bg-purple-dark" style="margin-top:20px ">
       <el-menu
         router :default-active="this.$route.path"
         default-active="2"
@@ -12,13 +14,15 @@
         background-color="#545c64"
         text-color="#fff"
         active-text-color="#ffd04b">
+        <el-menu-item-group>
           <el-menu-item index="/sAdmin_Message" @click="chaxun">导入人才信息</el-menu-item>
         <el-menu-item index="/sAdmin_Export" @click="chaxun">导出人才信息</el-menu-item>
         <el-menu-item index="/sAdmin_Introduction" @click="chaxun">高校简介</el-menu-item>
+        </el-menu-item-group>
       </el-menu>
     </div></el-col>
 <!--    右边显示内容-->
-    <el-col :span="18"><div class="grid-content bg-purple-dark">
+    <el-col :span="18"><div class="grid-content bg-purple-dark" style="margin-top:20px">
       <router-view></router-view>
     </div></el-col>
   </el-row>
@@ -46,6 +50,9 @@ export default {
         console.log(err)
       })
     },
+    exitLogin:function (){
+      this.$router.replace('/school_login')
+    }
   }
 }
 </script>
@@ -53,6 +60,7 @@ export default {
 <style scoped>
 .bg-purple-dark {
   background: white;
+
 }
 .grid-content {
   border-radius: 4px;
